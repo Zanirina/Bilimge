553a613293a6d6a8e18ac1cfe8c884374e54c8cd
Zanirina <beketovazarina.bz@gmail.com>
2026-03-27 22:48:11 +0500

    first endpoint Universities

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'main',
+    'rest_framework',
     'unipage',
     'django.contrib.admin',
     'django.contrib.auth',
--- backend/unipage/views.py
+++ backend/unipage/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import University
+from .serializers import UniversitySerializer
 
-# Create your views here.
+
+class UniversityViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = University.objects.all()
+    serializer_class = UniversitySerializer
+
 
 def unipage(request):
     return render(request, 'main/unipage.html')
