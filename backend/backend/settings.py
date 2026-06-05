a5ccf33433e47e7e4b1cc64ed64c7300f3e25a26
Zanirina <beketovazarina.bz@gmail.com>
2026-06-05 15:36:35 +0500

    repare for deploy

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -80,7 +80,7 @@ if os.getenv('DATABASE_URL'):
         'default': dj_database_url.config(
             default=os.getenv('DATABASE_URL'),
             conn_max_age=600,
-            ssl_require=True,
+            # ssl_require=True,
         )
     }
 else:
