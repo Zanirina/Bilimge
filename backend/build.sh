7c494b479f3cec0f3b723d4ec4bec291e8a13867
Zanirina <beketovazarina.bz@gmail.com>
2026-06-07 17:39:25 +0500

    add loggins

--- backend/build.sh
+++ backend/build.sh
@@ -7,4 +7,3 @@ python manage.py collectstatic --no-input
 
 python manage.py migrate --fake-initial
 
-echo "from django.contrib.auth import get_user_model; U = get_user_model(); U.objects.filter(email='admin@bilimge.kz').exists() or U.objects.create_superuser('admin@bilimge.kz', 'Admin123!')" | python manage.py shell
