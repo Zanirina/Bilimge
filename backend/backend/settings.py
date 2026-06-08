e40e3816c5d79dde93d6a88ec7c1afee7fb0c407
Zanirina <beketovazarina.bz@gmail.com>
2026-06-08 22:49:37 +0500

    notification fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -168,7 +168,7 @@ else:
     STATIC_ROOT = BASE_DIR / 'staticfiles'
 AUTH_USER_MODEL = 'userpage.User'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
